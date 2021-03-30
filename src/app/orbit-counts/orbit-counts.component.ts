ee9811bfdd40701cb2c5889f44d936e6144b93e0
Ben <bensztukowski@gmail.com>
2021-03-30 12:02:59 -0500

    Finished bonus missions

---
+++ src/app/orbit-counts/orbit-counts.component.ts
@@ -0,0 +1,25 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Satellite } from '../satellite';
+
+@Component({
+  selector: 'app-orbit-counts',
+  templateUrl: './orbit-counts.component.html',
+  styleUrls: ['./orbit-counts.component.css']
+})
+export class OrbitCountsComponent implements OnInit {
+  @Input() satellites: Satellite[];
+  constructor() { }
+
+  ngOnInit() {
+  }
+  count(term: string):number{
+    let count: number = 0;
+    for (let i = 0; i < this.satellites.length; i++){
+      if (this.satellites[i].type === term){
+        count +=1;
+      }
+    }
+    return count;
+  }
+
+}
